10e97bf09e898e890cb63e479bcfbaae2dd00f19
Scott Roberson <sroberson@gmail.com>
2015-04-30 14:43:33 -0400

    changed plugin to handle a directory of files, instead of direct file listing

--- tasks/inline_css.js
+++ tasks/inline_css.js
@@ -8,71 +8,80 @@
 
 'use strict';
 
+var juice = require('juice')
+    , path = require('path')
+    , async = require('async')
+    , count
+    , done;
+
+
 module.exports = function (grunt) {
 
-    // Please see the Grunt documentation for more information regarding task
-    // creation: http://gruntjs.com/creating-tasks
+    var options = {
+        underscoresAsPartials: true
+    };
 
-    var juice = require('juice');
+    var compileInlineFile = function (filepath, destination, nextFile, options) {
+
+        /*
+            This plugin has been changed to handle a directory globbing pattern,
+            instead of having to list out the files directly.
+
+            The directory listing is working, but the single file does not currently work.
+        */
+        var juicedFile,
+            outputFilename = path.resolve(destination, path.basename(filepath));
+
+        grunt.log.debug("outputfile is " + outputFilename);
+
+        juicedFile = juice(grunt.file.read(filepath), options);
+
+        //grunt.log.debug("Derived html: " + juicedFile);
+        grunt.log.ok("Writing to : " + path.basename(filepath));
+
+        if (juicedFile) {
+            grunt.file.write(outputFilename, juicedFile );
+            count++;
+        } else {
+            return grunt.log.error("File " + outputFilename + " error");
+            return false;
+        }
+        nextFile();
+    };
 
     grunt.registerMultiTask('inlinecss', 'Takes an html file with css link and turns inline.  Great for emails.', function () {
-        // Merge task-specific and/or target-specific options with these defaults.
-        var options = this.options();
+
+        var opts = grunt.util._.defaults(this.options(), options);
+        count = 0;
+
         var done = this.async();
-        var index = 0;
-        var count = this.files.length;
 
-        // Iterate over all specified file groups.
-        this.files.forEach(function (f) {
+        async.each(this.files, function(fileGlob, nextGlob) {
 
-            var filepath = f.src.toString();
-            if (typeof filepath !== 'string' || filepath === '') {
-                grunt.log.error('src must be a single string');
-                return false;
-            }
+            var destination = fileGlob.dest;
+            grunt.log.debug("FileGlob: " + fileGlob.src);
 
-            if (!grunt.file.exists(filepath)) {
-                grunt.log.error('Source file "' + filepath + '" not found.');
-                return false;
-            }
+            async.each(fileGlob.src, function(filepath, nextFile) {
 
-            /**
-             * Updated - Juice 1.0.0 compatible
-             * TODO: could use a bit of work
-             */
-            var juicedFile = juice(grunt.file.read(filepath), options);
-
-            if (juicedFile) {
-                grunt.file.write(f.dest, juicedFile);
-                grunt.log.writeln('File "' + f.dest + '" created.');
-                index++;
-                if (index === count) done();
-            } else {
-                return grunt.log.error("File " + f.dest + " error");
-                return false;
-            }
-
-            /**
-             * Original - Juice 0.4.0 compatible
-            juice(filepath, options, function (err, html) {
-
-                if (err) {
-                    return grunt.log.error(err);
+                if (grunt.file.exists(filepath) || grunt.file.exists(filepath)) {
+                    compileInlineFile(filepath, destination, nextFile, opts);
+                } else {
+                    nextGlob();
                 }
 
-                grunt.file.write(f.dest, html);
-                grunt.log.writeln('File "' + f.dest + '" created.');
-
-
-                index++;
-                if (index === count) {
-                    done();
-                }
-
+            }, function() {
+                // When we are done with all files in this glob
+                // continue to the next glob
+                nextGlob();
             });
-            */
-
+        }, function() {
+            // When we are done with all globs
+            // call done to tell the Grunt task we are done
+            done();
         });
+
+        grunt.log.ok("Compiled " + count + " files.");
+
     });
 
 };
